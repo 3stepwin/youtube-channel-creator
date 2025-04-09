b3d6b22601ff685b96f3b2c28e5dc04af7345358
garfield <3stepwin@gmail.com>
2025-04-08 20:44:28 -0400

    Initial project setup with frontend and backend

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,6 +1,15 @@
+<<<<<<< HEAD
 module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {}
   }
 }
+=======
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {}
+  }
+}
+>>>>>>> 56d83b3 (Initial project setup with frontend and backend)
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -8,4 +9,16 @@ module.exports = {
     extend: {},
   },
   plugins: [],
+=======
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+>>>>>>> 56d83b3 (Initial project setup with frontend and backend)
 }
